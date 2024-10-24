0919da667b287922c9a89ad7230487b97e752c6f
nagase <nagaken8937@gmail.com>
2024-10-24 23:01:31 +0900

    シュミレート2

--- Assets/Scripts/TargetPoint 1/TargetPointCreater.cs
+++ Assets/Scripts/TargetPoint 1/TargetPointCreater.cs
@@ -6,28 +6,21 @@ namespace TargetPosition2
     public class TargetPointCreater : MonoBehaviour
     {
         [SerializeField]
+        private float t = 0;
+
+        [SerializeField]
         private Transform start;
 
         [SerializeField]
         private Transform end;
 
-        private CalcYPosition calcYPosition = null;
-
-        private CalcXPosition calcXPosition = null;
-
         [SerializeField]
         private GameObject prefab = null;
 
-        private List<GameObject> objs = new();
+        private readonly List<GameObject> objs = new();
 
         [SerializeField, Range(0.016f, 0.16f)]
-        private float duration;
-
-        private void Awake()
-        {
-            calcYPosition = new CalcYPosition();
-            calcXPosition = new CalcXPosition();
-        }
+        private float pointDuration;
 
         private void Update()
         {
@@ -36,22 +29,26 @@ namespace TargetPosition2
                 objs[i].SetActive(false);
             }
 
-            // y,z軸固定
-            float distance = end.position.x - start.position.x;
-
             int count = 0;
-            for (float i = 0; i <= 1; i += duration)
-            {
-                var y = calcYPosition.GetPositionY(start.position, end.position.y, i);
-                var x = calcXPosition.GetPositionX(start.position, distance, calcYPosition.FinalTime, i);
 
+            var startPos = start.position; // 初期位置
+            var diffY = (end.position - startPos).y; // 始点と終点のy成分の差分
+            var vn = (diffY - Physics2D.gravity.y * 0.5f * t * t) / t; // 鉛直方向の初速度vn
+            float endT = t;
+
+            // 放物運動
+            for (var t = 0f; t < endT; t += pointDuration)
+            {
                 if (count >= objs.Count)
                 {
                     objs.Add(Instantiate(prefab));
                 }
 
+                var p = Vector3.Lerp(startPos, end.position, t / endT);   //水平方向の座標を求める (x,z座標)
+                p.y = startPos.y + vn * t + 0.5f * Physics2D.gravity.y * t * t; // 鉛直方向の座標 y
+
                 objs[count].SetActive(true);
-                objs[count].transform.position = new Vector3(x, y, 0);
+                objs[count].transform.position = p;
                 count++;
             }
         }
